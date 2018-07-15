8f3a9fc193a889974a590f537f7029ac9d6aee4d
groovybytes <christian.huber@groovybytes.at>
2018-07-15 22:03:05 +0200

    dffdfsggf

--- src/app/angular-daw/model/project/Project.ts
+++ src/app/angular-daw/model/project/Project.ts
@@ -3,8 +3,23 @@ import {TimeSignature} from "../mip/TimeSignature";
 
 
 export class Project{
-  //header: MidiFileHeader;
-  bpm:number;
-  signature:TimeSignature;
+  get bpm(): number {
+    return this._bpm;
+  }
+
+  set bpm(value: number) {
+    this._bpm = value;
+  }
+
+  get signature(): TimeSignature {
+    return this._signature;
+  }
+
+  set signature(value: TimeSignature) {
+    this._signature = value;
+  }
+  private _bpm:number;
+  private _signature:TimeSignature;
   tracks:Array<Track>=[];
+
 }
