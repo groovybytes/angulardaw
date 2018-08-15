a3c122d493dec40df489b51351bd5e6133f56d56
christian.huber <christian.huber@groovybytes.at>
2018-08-15 15:31:41 +0200

    zzzzz

---
+++ src/app/daw-grid/daw-grid.component.ts
@@ -0,0 +1,77 @@
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {PluginId} from "../model/daw/plugins/PluginId";
+import {PatternViewModel} from "../model/viewmodel/PatternViewModel";
+import {FlexyGridEntry} from "../ui/flexytable/model/FlexyGridEntry";
+import {DawGridService} from "./daw-grid.service";
+import {GridCellViewModel} from "../model/viewmodel/GridCellViewModel";
+import {ContentCell} from "../ui/flexytable/model/ContentCell";
+import {HeaderCell} from "../ui/flexytable/model/HeaderCell";
+import {TrackViewModel} from "../model/viewmodel/TrackViewModel";
+import {ProjectViewModel} from "../model/viewmodel/ProjectViewModel";
+
+@Component({
+  selector: 'daw-grid',
+  templateUrl: './daw-grid.component.html',
+  styleUrls: ['./daw-grid.component.scss']
+})
+export class DawGridComponent implements OnInit, OnChanges {
+
+  @Input() project: ProjectViewModel;
+  plugins: Array<string> = Object.keys(PluginId);
+
+
+  @Input() cellWidth: number;
+  @Input() cellHeight: number;
+  @Output() focusedPatternChanged: EventEmitter<{pattern:PatternViewModel,trackId:string}> = new EventEmitter();
+  @Output() pluginChanged: EventEmitter<TrackViewModel> = new EventEmitter<TrackViewModel>();
+  @Output() trackAdded: EventEmitter<TrackViewModel> = new EventEmitter<TrackViewModel>();
+  @Output() trackRemoved: EventEmitter<TrackViewModel> = new EventEmitter<TrackViewModel>();
+
+  constructor(private gridService: DawGridService) {
+  }
+
+  ngOnInit() {
+
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.project && changes.project.currentValue) {
+      if (this.project.grid.headerCells.length === 0) this.project.grid.headerCells = this.gridService.createHeaderCells(this.project.grid.nColumns);
+      if (this.project.grid.contentCells.length === 0) this.project.grid.contentCells = this.gridService.createContentCells(this.project.grid.nRows, this.project.grid.nColumns);
+      this.gridService.updateEntryPositions(this.project.grid, this.cellWidth, this.cellHeight);
+    }
+  }
+
+  onGridEntryAdded(entry: FlexyGridEntry<GridCellViewModel>): void {
+    let newPattern = this.gridService.addEvent(entry, this.cellWidth, this.cellHeight, this.project.patterns);
+    this.gridService.changePattern(this.project,entry,this.focusedPatternChanged);
+
+  }
+
+  onGridEntryRemoved(entry: FlexyGridEntry<GridCellViewModel>): void {
+    this.gridService.removeEvent(entry, this.project.patterns);
+    this.focusedPatternChanged.emit(null);
+  }
+
+  onGridEntryUpdated(entry: FlexyGridEntry<GridCellViewModel>): void {
+    this.gridService.updateEvent(entry, this.cellWidth, this.cellHeight);
+  }
+
+  onEntryClicked(entry: FlexyGridEntry<GridCellViewModel>): void {
+    this.gridService.changePattern(this.project,entry,this.focusedPatternChanged);
+  }
+
+  onCellClicked(cell: ContentCell): void {
+    this.gridService.changePattern(this.project,null,this.focusedPatternChanged);
+
+  }
+
+  setPlugin(pluginId: PluginId, cell: HeaderCell<TrackViewModel>): void {
+    let trackAdded = this.gridService.setPlugin(pluginId, cell, this.project);
+    if (trackAdded) this.trackAdded.emit(cell.data);
+    this.pluginChanged.emit(cell.data);
+  }
+
+
+}
---
+++ src/app/ui/flexytable/flexytable.component.ts
@@ -0,0 +1,116 @@
+import {AfterViewInit, Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
+import {ContentCell} from "./model/ContentCell";
+import {HeaderCell} from "./model/HeaderCell";
+import {FlexyGridEntry} from "./model/FlexyGridEntry";
+import DraggableOptions = JQueryUI.DraggableOptions;
+import DraggableEventUIParams = JQueryUI.DraggableEventUIParams;
+
+@Component({
+  selector: 'flexytable',
+  templateUrl: './flexytable.component.html',
+  styleUrls: ['./flexytable.component.scss']
+})
+export class FlexytableComponent<T> implements OnInit, AfterViewInit {
+
+  @Input() cellHeight: number = 50;
+  @Input() cellWidth: number = 100;
+  @Input() contentCells: Array<Array<ContentCell>> = [];
+  @Input() headerCells: Array<HeaderCell<any>> = [];
+  @Input() entries: Array<FlexyGridEntry<T>> = [];
+
+  @Output() entryAdded: EventEmitter<FlexyGridEntry<T>> = new EventEmitter();
+  @Output() entryUpdated: EventEmitter<FlexyGridEntry<T>> = new EventEmitter();
+  @Output() entryRemoved: EventEmitter<FlexyGridEntry<T>> = new EventEmitter();
+  @Output() entryClicked: EventEmitter<FlexyGridEntry<T>> = new EventEmitter();
+  @Output() cellClicked: EventEmitter<ContentCell> = new EventEmitter();
+
+  @HostListener('window:keydown', ['$event'])
+  keyDown(event: KeyboardEvent) {
+    if (event.which === 16) this.useGrid = false;
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyUp(event: KeyboardEvent) {
+    if (event.which === 16) this.useGrid = true;
+  }
+
+  grid: Array<number>;
+  useGrid: boolean = true;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    /* this.headerCells = new Array(10).fill(new HeaderCell());
+     this.contentCells = [];
+     for (let i = 0; i < 100; i++) {
+       this.contentCells.push([]);
+       for (let j = 0; j < 10; j++) {
+         this.contentCells[i].push(new ContentCell(i, j));
+       }
+     }*/
+  }
+
+  onContentCellDblClicked(cell: ContentCell): void {
+    let newEntry = new FlexyGridEntry(null, cell.column * this.cellWidth, cell.row * this.cellHeight);
+    this.entries.push(newEntry);
+    this.entryAdded.emit(newEntry);
+
+
+  }
+
+  onEntryDblClicked(entry: FlexyGridEntry<any>): void {
+    let index = this.entries.indexOf(entry);
+    this.entries.splice(index, 1);
+    this.entryRemoved.emit(entry);
+  }
+
+  onEntryClicked(entry: FlexyGridEntry<any>): void {
+    this.entryClicked.emit(entry);
+  }
+
+  onCellClicked(cell: ContentCell): void {
+    this.cellClicked.emit(cell);
+  }
+
+  getDragParams(entry: FlexyGridEntry<T>): DraggableOptions {
+    return {
+      scroll: false,
+      scrollSensitivity: 100,
+      scrollSpeed: 100,
+      containment: '#containment-wrapper',
+      grid: this.useGrid ? [this.cellWidth, this.cellHeight] : [1, this.cellHeight],
+      start: (event: Event, ui: DraggableEventUIParams) => {
+
+      },
+      drag: (event: Event, ui: DraggableEventUIParams) => {
+        entry.left = ui.position.left;
+        entry.top = ui.position.top;
+        this.entryUpdated.emit(entry);
+        //entry.isOffGrid=!this.useGrid;
+      }
+      , stop: (event: Event, ui: DraggableEventUIParams) => {
+        //$(event.target).css({"left":"0px"})
+      },
+    }
+  }
+
+  onEntryMouseDown(entry: FlexyGridEntry<T>, event): void {
+    /*  console.log($(event.target).position());
+      let element=$(event.target);
+      let left=element.position().left;
+      let newX=Math.round(left / 100) * 100;
+      console.log(element);
+      console.log(element.draggable("instance"));
+      entry.left=newX;*/
+
+  }
+
+  ngAfterViewInit(): void {
+    /*console.log($(".cell-content").first().width());
+    this.grid=[$(".cell-content").first().width(),50];*/
+  }
+
+
+}
